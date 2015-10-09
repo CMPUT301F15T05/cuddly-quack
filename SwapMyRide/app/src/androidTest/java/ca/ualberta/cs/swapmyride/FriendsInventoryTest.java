e4e4b5af5443e65041f58ad2d9162ab4ac3646c4
Carson McLean <camclean@ualberta.ca>
2015-10-09 12:36:47 -0600

    Adds FriendsInventoryTest. Use case #13 completed

---
+++ SwapMyRide/app/src/androidTest/java/ca/ualberta/cs/swapmyride/FriendsInventoryTest.java
@@ -0,0 +1,35 @@
+package ca.ualberta.cs.swapmyride;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * Created by carsonmclean on 9/10/15.
+ */
+public class FriendsInventoryTest {
+    public FriendsInventoryTest() {super(MainMenu.class); }
+
+    public void testSearchFriendsInventory() {
+        FriendsList friendsList = new FriendsList();
+        User user = new User();
+
+        user.addUser("camclean");
+        user.addUserName("Carson Mclean");
+        user.addUserEmail("camclean@ualberta.ca");
+        user.addUserAddress("300 Shirley Street");
+
+        Item item = new Item();
+
+        item.setPhoto(picture);
+        item.setName("Cadillac");
+        item.setCategory("Sedan");
+        item.setQuality("Good");
+        item.setQuantity(1);
+        item.setComments("1995 Cadillac");
+
+        user.addItem(item);
+
+        assertTrue(user.getInventory().search("Cadillac"));
+        assertTrue(user.getInventory().getCategory("Sedan").size() == 1);
+    }
+}
