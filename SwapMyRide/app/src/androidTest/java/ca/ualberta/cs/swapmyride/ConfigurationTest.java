9776c79b60b735128ead845bf88ca33c926899eb
Garry Bullock <gbullock@ualberta.ca>
2015-10-08 21:12:05 -0600

    Added Image Preference test

--- SwapMyRide/app/src/androidTest/java/ca/ualberta/cs/swapmyride/ConfigurationTest.java
+++ SwapMyRide/app/src/androidTest/java/ca/ualberta/cs/swapmyride/ConfigurationTest.java
@@ -9,5 +9,21 @@ public class ConfigurationTest extends ActivityInstrumentationTestCase2{
     public ConfigurationTest(){
         super(MainMenu.class);
     }
-    
+
+    public void testImagePreferance(){
+        User user = new User();
+        user.setDownloadImages(false);
+        Image image = new Image();
+        //autoDownloadImage should return true if the image downloads
+        boolean downloaded = autoDownloadImage(image);
+        assertFalse(downloaded);
+        //when the user selects to download an image, it should download
+        downloaded = manualDownloadImage(image);
+        assertTrue(downloaded);
+    }
+
+    public void testEditProfile(){
+        
+    }
+
 }
