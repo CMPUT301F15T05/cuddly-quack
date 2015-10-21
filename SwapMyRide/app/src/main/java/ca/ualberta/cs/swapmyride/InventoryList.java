4a6e6d1ac366891a0b763dd5476774e9534a3911
Garry Bullock <gbullock@ualberta.ca>
2015-10-21 16:45:05 -0600

    Added InventoryList class

---
+++ SwapMyRide/app/src/main/java/ca/ualberta/cs/swapmyride/InventoryList.java
@@ -0,0 +1,10 @@
+package ca.ualberta.cs.swapmyride;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Garry on 2015-10-21.
+ */
+public class InventoryList {
+    private ArrayList<Vehicle> vehicleList;
+}
