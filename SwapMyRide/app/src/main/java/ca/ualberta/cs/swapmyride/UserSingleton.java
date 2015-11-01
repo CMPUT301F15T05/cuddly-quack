ef2d23e5c7d0f188bea6b986c28642f13f273e54
Adriano Marini <marini@ualberta.ca>
2015-11-01 14:54:54 -0700

    Implemented logging in. Using UserSingleton to store user names, authenticate by username. For now, if the username doesn't exist, the user is forced to sign up.

--- SwapMyRide/app/src/main/java/ca/ualberta/cs/swapmyride/UserSingleton.java
+++ SwapMyRide/app/src/main/java/ca/ualberta/cs/swapmyride/UserSingleton.java
@@ -1,10 +1,13 @@
 package ca.ualberta.cs.swapmyride;
 
+import java.util.ArrayList;
+
 /**
  * Created by adrianomarini on 2015-11-01.
  */
 public class UserSingleton {
     private static User currentUser;
+    private static ArrayList<User> users = new ArrayList<>();
 
     private static UserSingleton ourInstance = new UserSingleton();
 
@@ -15,12 +18,33 @@ public class UserSingleton {
     private UserSingleton() {
     }
 
-    public static void addUser(User user){
-        currentUser = user;
+    public static void addCurrentUser(String username){
+        String tempString;
+        for(User user : users) {
+            tempString = user.getUser();
+            if (tempString.equals(username)) {
+                currentUser = user;
+            }
+        }
     }
 
     public static User getCurrentUser(){
         return currentUser;
+    }
+
+    public static void addUser(User user){
+        users.add(user);
+    }
+
+    public static boolean userExists(String username){
+        String tempString;
+        for(User user : users){
+            tempString = user.getUser();
+            if(tempString.equals(username)){
+                return true;
+            }
+        }
+        return false;
     }
 
 }
