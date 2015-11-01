2c094dfba85d5cd7a36d818123d87852df8c85b5
GarryBullock <gbullock@ualberta.ca>
2015-11-01 16:45:00 -0700

    Add SearchController with method declarations and TODO's

---
+++ SwapMyRide/app/src/main/java/ca/ualberta/cs/swapmyride/SearchController.java
@@ -0,0 +1,19 @@
+package ca.ualberta.cs.swapmyride;
+
+/**
+ * Created by Garry on 2015-11-01.
+ */
+public class SearchController {
+
+    public User findUser(String username){
+        //TODO: Search the server for a user that matches the given username
+        return UserSingleton.getCurrentUser();
+    }
+
+    public Vehicle findVehicle(String vehicleName){
+        //TODO: Search the server for a vehicle that matches the given vehicleName
+        return new Vehicle();
+    }
+
+    
+}
--- SwapMyRide/app/src/main/java/ca/ualberta/cs/swapmyride/TradesController.java
+++ SwapMyRide/app/src/main/java/ca/ualberta/cs/swapmyride/TradesController.java
@@ -13,5 +13,5 @@ public class TradesController {
         return UserSingleton.getCurrentUser().getPastTrades();
     }
 
-    
+
 }
