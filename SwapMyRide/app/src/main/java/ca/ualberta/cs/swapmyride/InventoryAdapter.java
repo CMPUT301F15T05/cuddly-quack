99465450f187e37a176a28e4b9914cffea678e76
dhaberst <daniel.haberstock1@gmail.com>
2015-10-31 18:17:12 -0600

    Added functionality to invetory listview!

--- SwapMyRide/app/src/main/java/ca/ualberta/cs/swapmyride/InventoryAdapter.java
+++ SwapMyRide/app/src/main/java/ca/ualberta/cs/swapmyride/InventoryAdapter.java
@@ -1,9 +1,42 @@
 package ca.ualberta.cs.swapmyride;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 /**
  * Created by Daniel on 2015-10-31.
  */
-public class InventoryAdapter {
 
-    
+// https://github.com/codepath/android_guides/wiki/Using-an-ArrayAdapter-with-ListView
+public class InventoryAdapter extends ArrayAdapter<Vehicle> {
+    public InventoryAdapter(Context context, ArrayList<Vehicle> vehicle) {
+        super(context, 0, vehicle);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        Vehicle vehicle = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.userinventoryitem, parent, false);
+        }
+        // Lookup view for data population
+        TextView vehicleTitle = (TextView) convertView.findViewById(R.id.vehicleTitle);
+        TextView quantity = (TextView) convertView.findViewById(R.id.quantity);
+        TextView ispublic = (TextView) convertView.findViewById(R.id.ispublic);
+
+        // Populate the data into the template view using the data object
+        vehicleTitle.setText(vehicle.getName());
+        quantity.setText(vehicle.getQuantity().toString());
+        ispublic.setText(vehicle.getPublic().toString());
+        // Return the completed view to render on screen
+        return convertView;
+    }
 }
--- SwapMyRide/app/src/main/java/ca/ualberta/cs/swapmyride/Tab2.java
+++ SwapMyRide/app/src/main/java/ca/ualberta/cs/swapmyride/Tab2.java
@@ -11,15 +11,58 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class Tab2 extends Fragment {
 
-    ListView invetory;
+    ListView inventory;
+    ArrayList<Vehicle> arrayOfVehicle;
+    InventoryList inventoryList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v =inflater.inflate(R.layout.tab2,container,false);
 
-        invetory = (ListView) v.findViewById(R.id.inventoryView);
+        inventoryList = new InventoryList();
+
+        // Testing
+        Vehicle vehicle = new Vehicle();
+        vehicle.setName("CoolCar");
+        vehicle.setPublic(true);
+        vehicle.setQuantity(2);
+        inventoryList.add(vehicle);
+
+        Vehicle vehicle2 = new Vehicle();
+        vehicle2.setName("CoolCar2");
+        vehicle2.setPublic(true);
+        vehicle2.setQuantity(2);
+        inventoryList.add(vehicle2);
+
+        Vehicle vehicle3 = new Vehicle();
+        vehicle3.setName("CoolCar3");
+        vehicle3.setPublic(true);
+        vehicle3.setQuantity(2);
+        inventoryList.add(vehicle3);
+
+        Vehicle vehicle4 = new Vehicle();
+        vehicle4.setName("CoolCar4");
+        vehicle4.setPublic(true);
+        vehicle4.setQuantity(2);
+        inventoryList.add(vehicle4);
+
+        Vehicle vehicle5 = new Vehicle();
+        vehicle5.setName("CoolCar5");
+        vehicle5.setPublic(true);
+        vehicle5.setQuantity(2);
+        inventoryList.add(vehicle5);
+
+        arrayOfVehicle = inventoryList.getList();
+
+        InventoryAdapter adapter = new InventoryAdapter(getActivity(), arrayOfVehicle);
+
+        inventory = (ListView) v.findViewById(R.id.inventoryView);
+
+        inventory.setAdapter(adapter);
 
         return v;
     }
