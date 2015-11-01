f3af0052a6abc99ed4906b0d03a6c65530c0f910
dhaberst <daniel.haberstock1@gmail.com>
2015-11-01 13:33:49 -0700

    Added defaultcar.png to drawable.

--- SwapMyRide/app/src/main/java/ca/ualberta/cs/swapmyride/ViewFriendsActivity.java
+++ SwapMyRide/app/src/main/java/ca/ualberta/cs/swapmyride/ViewFriendsActivity.java
@@ -1,12 +1,11 @@
 package ca.ualberta.cs.swapmyride;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.View;
+import android.view.MenuItem;
 
 public class ViewFriendsActivity extends AppCompatActivity {
 
@@ -26,6 +25,23 @@ public class ViewFriendsActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(ViewFriendsActivity.this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
