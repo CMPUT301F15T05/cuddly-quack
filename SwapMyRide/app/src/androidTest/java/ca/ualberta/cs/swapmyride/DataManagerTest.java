08d69c076df65451813e4e45ef7871b55202474c
Garry Bullock <gbullock@ualberta.ca>
2015-11-02 16:27:51 -0700

    Attempting to fix the DataManager tests (Null Pointer in Tab2 Activity)

--- SwapMyRide/app/src/androidTest/java/ca/ualberta/cs/swapmyride/DataManagerTest.java
+++ SwapMyRide/app/src/androidTest/java/ca/ualberta/cs/swapmyride/DataManagerTest.java
@@ -1,5 +1,7 @@
 package ca.ualberta.cs.swapmyride;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
 import android.util.Log;
 
@@ -17,7 +19,7 @@ public class DataManagerTest extends ActivityInstrumentationTestCase2 {
         user.setUserName("gbullock");
         user.setUserEmail("gbullock@ualbert.ca");
         Log.i("FilePath", getActivity().getBaseContext().getFileStreamPath(user.getUserName()).toString());
-
+        UserSingleton.addUser(user);
         dataManager.saveUser(user);
 
         User loadTo = dataManager.loadUser("gbullock");
@@ -45,5 +47,45 @@ public class DataManagerTest extends ActivityInstrumentationTestCase2 {
 
         //check that the file no longer exists
         assertFalse(getActivity().getBaseContext().getFileStreamPath(user.getUserName()).exists());
+    }
+
+    public void testSaveUserWithVehicle(){
+        DataManager dataManager = new DataManager(getActivity());
+
+        User user = new User();
+        user.setName("Garry");
+        user.setUserAddress("4465");
+        user.setUserName("gbullock");
+        user.setUserEmail("gbullock@ualbert.ca");
+        UserSingleton.addUser(user);
+        Log.i("FilePath", getActivity().getBaseContext().getFileStreamPath(user.getUserName()).toString());
+        assertTrue(getActivity().getBaseContext().getFileStreamPath(user.getUserName()).exists());
+
+        dataManager.saveUser(user);
+
+        User loadTo = dataManager.loadUser("gbullock");
+
+        assertTrue(loadTo.getName().equals(user.getName()));
+        assertTrue(loadTo.getUserEmail().equals(user.getUserEmail()));
+        assertTrue(loadTo.getUserAddress().equals(user.getUserAddress()));
+        assertTrue(loadTo.getUserName().equals(user.getUserName()));
+
+        Vehicle car = new Vehicle();
+        car.setName("2008 Cadillac");
+        car.setCategory(VehicleCategory.SEDAN);
+        car.setQuality(VehicleQuality.OKAY);
+        car.setQuantity(1);
+        car.setPublic(true);
+
+        user.addItem(car);
+
+        dataManager.saveUser(user);
+
+        loadTo = dataManager.loadUser("gbullock");
+
+        //check that the list exists
+        assertTrue(loadTo.getInventory().size() == 1);
+        //check the given car is the same as the car we gave it
+        assertTrue(loadTo.getInventory().getList().get(0).equals(car));
     }
 }
