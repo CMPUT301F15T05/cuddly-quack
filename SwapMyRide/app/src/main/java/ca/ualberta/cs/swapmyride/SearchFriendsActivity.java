439b8b95c61949ba5113f373668d026c2ef15101
dhaberst <daniel.haberstock1@gmail.com>
2015-11-03 20:06:23 -0700

    Added findviews into searchFriendsActivity!

--- SwapMyRide/app/src/main/java/ca/ualberta/cs/swapmyride/SearchFriendsActivity.java
+++ SwapMyRide/app/src/main/java/ca/ualberta/cs/swapmyride/SearchFriendsActivity.java
@@ -1,12 +1,19 @@
 package ca.ualberta.cs.swapmyride;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
 public class SearchFriendsActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    EditText searchField;
+    ImageButton searchButton;
+    ListView friendView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +22,12 @@ public class SearchFriendsActivity extends AppCompatActivity {
 
         toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
+
+        searchField = (EditText) findViewById(R.id.searchField);
+        searchButton = (ImageButton) findViewById(R.id.searchButton);
+        friendView = (ListView) findViewById(R.id.friendView);
+
+
     }
 
 }
