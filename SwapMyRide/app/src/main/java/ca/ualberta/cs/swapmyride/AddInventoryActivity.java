1bc6d4ff501b595c8833bb9e56f3d54641ceda85
dhaberst <daniel.haberstock1@gmail.com>
2015-11-05 21:58:08 -0700

    Added default car image as well as fiddled with a couple of layouts.

--- SwapMyRide/app/src/main/java/ca/ualberta/cs/swapmyride/AddInventoryActivity.java
+++ SwapMyRide/app/src/main/java/ca/ualberta/cs/swapmyride/AddInventoryActivity.java
@@ -300,31 +300,6 @@ public class AddInventoryActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_CAMERA: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
-        }
-    }
-
     /**
      * After returning from the camera activity, get the photo information
      * and send it into the structure to get it ready.
