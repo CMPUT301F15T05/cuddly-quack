e1e775a3233e09ca6982c64a6e74b28e1b35c4f0
Adriano Marini <marini@ualberta.ca>
2015-11-18 19:15:34 -0700

    Made file for MainMenu test -- to be implemented

---
+++ SwapMyRide/app/src/androidTest/java/ca/ualberta/cs/swapmyride/MainMenuTest.java
@@ -0,0 +1,22 @@
+package ca.ualberta.cs.swapmyride;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import ca.ualberta.cs.swapmyride.View.MainMenu;
+
+/**
+ * Created by adrianomarini on 2015-11-18.
+ *
+ * In this test, we will ensure that the main menu functions
+ * correctly and that all of the intended functions are present
+ * and operational
+ *
+ */
+public class MainMenuTest extends ActivityInstrumentationTestCase2 {
+
+    public MainMenuTest() { super(MainMenu.class); }
+
+    public void testMainMenu(){
+        
+    }
+}
