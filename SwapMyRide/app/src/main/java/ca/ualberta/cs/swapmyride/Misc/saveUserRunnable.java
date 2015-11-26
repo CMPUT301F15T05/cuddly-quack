5910d8ade77b59479dc25af19657abec4e4cbdd0
GarryBullock <gbullock@ualberta.ca>
2015-11-25 22:53:48 -0700

    Added dependancy on legacy http library "useLibrary 'org.apache.http.legacy'" in app build.gradle

--- SwapMyRide/app/src/main/java/ca/ualberta/cs/swapmyride/Misc/saveUserRunnable.java
+++ SwapMyRide/app/src/main/java/ca/ualberta/cs/swapmyride/Misc/saveUserRunnable.java
@@ -1,7 +1,9 @@
 package ca.ualberta.cs.swapmyride.Misc;
 
-import ca.ualberta.cs.swapmyride.Model.User;
+import android.util.Log;
 
+import ca.ualberta.cs.swapmyride.Model.User;
+import org.apache.http.HttpEntity;
 
 /**
  * Created by Garry on 2015-11-25.
@@ -9,9 +11,11 @@ import ca.ualberta.cs.swapmyride.Model.User;
 public class SaveUserRunnable implements Runnable{
     private User user;
     private String url;
+    //private
     public SaveUserRunnable(final User user, final String url){
         this.user = user;
         this.url = url + "users/" + user.getUserName();
+        Log.d("USER URL", this.url);
     }
 
     public  void run(){
