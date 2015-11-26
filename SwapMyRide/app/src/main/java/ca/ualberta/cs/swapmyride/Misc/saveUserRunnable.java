9a9405254b7f02454b48ab268ada392d1475507e
GarryBullock <gbullock@ualberta.ca>
2015-11-25 22:27:02 -0700

    Refactor saveUserRunnable to SaveUserRunnable

--- SwapMyRide/app/src/main/java/ca/ualberta/cs/swapmyride/Misc/saveUserRunnable.java
+++ SwapMyRide/app/src/main/java/ca/ualberta/cs/swapmyride/Misc/saveUserRunnable.java
@@ -6,10 +6,10 @@ import ca.ualberta.cs.swapmyride.Model.User;
 /**
  * Created by Garry on 2015-11-25.
  */
-public class saveUserRunnable implements Runnable{
+public class SaveUserRunnable implements Runnable{
     private User user;
     private String url;
-    public saveUserRunnable(final User user, final String url){
+    public SaveUserRunnable(final User user, final String url){
         this.user = user;
         this.url = url + "users/" + user.getUserName();
     }
