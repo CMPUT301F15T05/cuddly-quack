d99ee4fd2cccec48433513df28290475aea820d0
GarryBullock <gbullock@ualberta.ca>
2015-11-27 17:03:27 -0700

    Completed main methods: Get, Save, Delete, SearchServer, and SearchLocal of new DataManager

--- SwapMyRide/app/src/main/java/ca/ualberta/cs/swapmyride/Controller/DataManager.java
+++ SwapMyRide/app/src/main/java/ca/ualberta/cs/swapmyride/Controller/DataManager.java
@@ -32,27 +32,52 @@ public class DataManager {
         ldm.saveUser(user);
     }
 
-    public User loadUser(String userName){
+    /**
+     * Loads the user from network if it is available, else it will attempt to load the user from
+     * disk...
+     * Returns a new user if it the given userName does not exist
+     * @param username
+     * @return
+     */
+    public User loadUser(String username){
+        if(networkAvailable()){
+            if (searchUserServer(username)){
+                return ndm.retrieveUser(username);
+            }
+        }
+        else if (ldm.searchUser(username)){
+            return ldm.loadUser(username);
+        }
+
         return new User();
     }
 
     public void deleteUser(String username){
+        if(networkAvailable()){
+            ndm.deleteUser(username);
+        }
+        ldm.deleteUser(username);
     }
 
-    public boolean searchUser(String username){
-        return true;
+    public boolean searchUserLocal(String username){
+        return ldm.searchUser(username);
     }
 
+    public boolean searchUserServer(String username){
+        if(networkAvailable()){
+            return ndm.searchUser(username);
+        }
+        else return false;
+    }
     /**
      * updateFriends gets friends from the server based on the friends list
      * of a user. This exists so that a friendsList does not store user
      * obejcts - but just usernames so that it is smaller.
-     *
      * @param user
      */
     public void updateFriends(User user){
         for (String friendUserName : user.getFriends().getFriendList()) {
-            if(searchUser(friendUserName)){
+            if(searchUserServer(friendUserName)){
                 //TODO Update the friends from server
             }
             else{
@@ -65,6 +90,11 @@ public class DataManager {
      * networkAvailable checks the android device to see if there is a network connection
      * available. It should be performed before doing any network action.
      * @return
+     *
+     * Taken from StackOverflow "Detect whether there is an Internet connection available on Android"
+     * Solution from user Alexandre Jasmine
+     * http://stackoverflow.com/questions/4238921/detect-whether-there-is-an-internet-connection-available-on-android
+     * November 27/2015
      */
     private boolean networkAvailable(){
         ConnectivityManager connectionManager = (ConnectivityManager)
