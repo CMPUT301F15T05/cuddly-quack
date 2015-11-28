56c154c15826336a32346932c1563139b9ecf212
dhaberst <daniel.haberstock1@gmail.com>
2015-11-28 15:28:37 -0700

    Took out redundant imports.

--- SwapMyRide/app/src/main/java/ca/ualberta/cs/swapmyride/View/ViewAPastTradeActivity.java
+++ SwapMyRide/app/src/main/java/ca/ualberta/cs/swapmyride/View/ViewAPastTradeActivity.java
@@ -1,12 +1,8 @@
 package ca.ualberta.cs.swapmyride.View;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
