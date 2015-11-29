c4d23a1f0a6f8b2e1dc1bbcb284c91045399416c
GarryBullock <gbullock@ualberta.ca>
2015-11-28 19:23:58 -0700

    Update EditProfileActivity to use new DataManager

--- SwapMyRide/app/src/main/java/ca/ualberta/cs/swapmyride/View/EditProfileActivity.java
+++ SwapMyRide/app/src/main/java/ca/ualberta/cs/swapmyride/View/EditProfileActivity.java
@@ -23,7 +23,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import ca.ualberta.cs.swapmyride.Controller.LocalDataManager;
+import ca.ualberta.cs.swapmyride.Controller.DataManager;
 import ca.ualberta.cs.swapmyride.Misc.UserSingleton;
 import ca.ualberta.cs.swapmyride.Model.User;
 import ca.ualberta.cs.swapmyride.R;
@@ -39,7 +39,7 @@ public class EditProfileActivity extends AppCompatActivity {
     UserSingleton userSingleton;
     User user;
     Button save;
-    LocalDataManager dataManager;
+    DataManager dataManager;
 
     /**
      * onCreate, the fields are initialized with the information that
@@ -58,6 +58,7 @@ public class EditProfileActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         user = userSingleton.getCurrentUser();
+        dataManager = new DataManager(EditProfileActivity.this);
 
         TextView usernameTextView = (TextView) findViewById(R.id.usernameField);
         usernameTextView.setText(user.getUserName().toString());
@@ -86,10 +87,7 @@ public class EditProfileActivity extends AppCompatActivity {
                 user.setName(nameTextView.getText().toString());
                 user.setUserEmail(emailTextView.getText().toString());
                 user.setUserAddress(addressTextView.getText().toString());
-
-                dataManager = new LocalDataManager(EditProfileActivity.this);
                 dataManager.saveUser(user);
-
                 finish();
             }
         });
